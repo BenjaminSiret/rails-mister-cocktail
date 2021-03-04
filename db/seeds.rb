2352d8e9afd26e035e18e8c3f82a7d82684353fb
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-04 12:40:46 +0100

    add cocktail controller and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Ingredient.destroy_all
+
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
@@ -5,3 +7,8 @@ Ingredient.create(name: 'apricot brandy')
 Ingredient.create(name: 'scotch')
 Ingredient.create(name: 'champagne')
 
+Cocktail.create(name: 'bloody mary')
+Cocktail.create(name: 'panache')
+Cocktail.create(name: 'Mojito')
+
+puts "Ingredients: #{Ingredient.count} - Cocktails: #{Cocktail.count}"
