74a5ba3a8c1fcfa509c21c703b0f4e4aae5f2543
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-04 18:06:32 +0100

    all features run, ugly design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
+lemon = Ingredient.create(name: 'lemon')
+ice = Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
 Ingredient.create(name: 'apricot brandy')
 Ingredient.create(name: 'scotch')
 Ingredient.create(name: 'champagne')
 
 Cocktail.create(name: 'bloody mary')
-Cocktail.create(name: 'panache')
-Cocktail.create(name: 'Mojito')
+panache = Cocktail.create(name: 'panache')
+mojito = Cocktail.create(name: 'Mojito')
 
-puts "Ingredients: #{Ingredient.count} - Cocktails: #{Cocktail.count}"
+Dose.create(ingredient: lemon, cocktail: mojito, description: '6 leaves')
+Dose.create(ingredient: ice, cocktail: panache, description: '9oz')
+
+puts "Ingredients: #{Ingredient.count} - Cocktails: #{Cocktail.count} - Doses: #{Dose.count}"
